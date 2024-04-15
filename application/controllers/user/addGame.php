33d2b1cf72017b240b696643e441c2eef9f0543e
siddesh <siddeshbhosale259@gmail.com>
2024-04-15 18:52:16 +0530

    3a commit

--- application/controllers/user/addGame.php
+++ application/controllers/user/addGame.php
@@ -25,12 +25,43 @@ class AddGame extends CI_Controller
                 echo json_encode(array("status"=>false ,"message"=>validation_errors()));
                 exit;
             }
+            else{
+                // $game=$this->input->post("gname");
+            // $tournament=$this->input->post("tname");
+            // $number_team =$this->input->post("nteam");
+            // $gender= $this->input->post("gender");
 
-            $game=$this->input->post("gname");
-            $tournament=$this->input->post("tname");
-            $number_team =$this->input->post("nteam");
-            $gender= $this->input->post("gender");
+            $data= array();
+            $data["gname"] = $this->input->post("gname");
+            $data["tname"] = $this->input->post("tname");
+            $data["nteam"] = $this->input->post("nteam");
+            $data["gender"] = $this->input->post("gender");
 
+            $config["upload_path"] = "./upload/gameupload/";
+            $config["allowed_types"] = "gif|jpg|jpeg|png";
+
+            $this->load->library('upload', $config);
+
+            $this->upload->initialize($config);
+
+            if (!$this->upload->do_upload('profile')) {
+
+                $error = array('error' => $this->upload->display_errors());
+                echo json_encode(array('error' => $error));
+                exit;
+            }
+
+            $dataa = $this->upload->data();
+
+            $img_path = base_url('upload/' . $dataa['file_name']);
+
+            $user_table = "user_dtl";
+            $address_table = "address_dtl";
+
+            $data["picture"] = $img_path;
+
+
+            }
 
 
         }
