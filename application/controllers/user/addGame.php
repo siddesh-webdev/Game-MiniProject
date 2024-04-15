3ef85e1c4422bed165a6b41e60e8478fd8f27393
siddesh <siddeshbhosale259@gmail.com>
2024-04-15 18:42:01 +0530

    saa commit

--- application/controllers/user/addGame.php
+++ application/controllers/user/addGame.php
@@ -11,8 +11,28 @@ class AddGame extends CI_Controller
     {
 
         if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
-            $this->input->post('');
-            
+            // $this->input->post('');
+
+            $this->form_validation->set_rules("gname", "game name", "trim|required");
+			$this->form_validation->set_rules("tname", "tname", "trim|required");
+			$this->form_validation->set_rules("nteam", "nteam", "trim|required");
+
+			$this->form_validation->set_rules("gender", "gender", "required");
+			$this->form_validation->set_rules("profile", "profile", "required");
+
+            if( $this->form_validation->run() == FALSE )
+            {
+                echo json_encode(array("status"=>false ,"message"=>validation_errors()));
+                exit;
+            }
+
+            $game=$this->input->post("gname");
+            $tournament=$this->input->post("tname");
+            $number_team =$this->input->post("nteam");
+            $gender= $this->input->post("gender");
+
+
+
         }
     }
 
